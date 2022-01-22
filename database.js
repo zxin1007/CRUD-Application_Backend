1f1b5ca4933f576f5f5d2ee9868bed7fe9723c66
Zixin Zou <zxinmm@gmail.com>
2022-01-21 19:08:21 -0500

    cors morgan express dependencie added

--- database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require ("sequelize")
-
-const sequelize = new Sequelize ('crud','postgres', 'yourpassword',{
-    host: 'localhost',
-    dialect: 'postgres'
-})
-
-module.exports = sequelize
--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const Sequelize = require('sequelize')
-const Student = require('./student')
-const Campus = require('./campus')
-const sequelize = require('./database')
+const express = require("express")
+const app = express()
+const PORT = 3000
+const morgan = require('morgan')
+const { db } = require('./server/database')
+const cors = require('cors')
 
-sequelize.authenticate().then(()=>{
-    console.log("success")
-}).catch(err=>{
-    console.log("error")
-})
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-Student.sync().then(()=>{
-    console.log("table and model synced success")
-}).catch(err=>{
-    console.log("error")
-})
-
-Campus.sync().then(()=>{
-    console.log("table and model synced success")
-}).catch(err=>{
-    console.log("error")
-})
+db.sync().then(() => {
+    console.log('db synced')
+    app.listen(PORT, () =>
+      console.log(`listening on port ${PORT}`)
+    )
+  })  
