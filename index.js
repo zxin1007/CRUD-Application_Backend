87b61c1c53270b398767ac087095f94dc2e6ebfb
Zixin Zou <zxinmm@gmail.com>
2022-01-21 20:16:55 -0500

    root index.js added and it connect to database and able to listen port

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const PORT = 3000
 const morgan = require('morgan')
 const { db } = require('./server/database')
 const cors = require('cors')
+const useApi = require('./server/api')
 
 app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api', useApi)
+
 db.sync().then(() => {
     console.log('db synced')
-    app.listen(PORT, () =>
+     app.listen(PORT, () =>
       console.log(`listening on port ${PORT}`)
-    )
-  })  
+     )
+})  
+
+// app.get("/api", async (req, res)=>{
+//   // res.send("musicians")
+//   await db.authenticate()
+//   const result = await db.query("select * from student")
+//   console.log(result)
+//   res.status(200).json({
+//     students : result,
+//   })
+
+// })
