d80f0b08468fc45d53f40c98889cb5769a6eabee
Zixin Zou <zxinmm@gmail.com>
2022-01-21 21:38:08 -0500

    single stdent view

--- server/api/student.js
+++ server/api/student.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
-const { Student, db } = require('../database/index')
+const { Student, Campus } = require('../database/index')
 
-router.get('/',async (req, res)=>{
+router.route('/')
+.get(async (req, res)=>{
     try{
         const student = await Student.findAll();
         res.send(student)
@@ -9,14 +10,35 @@ router.get('/',async (req, res)=>{
         console.log(err)
     }
 })
-router.post('/',async(req, res)=>{
+.post(async(req, res)=>{
     try{
         console.log(req.body)
-        // const newStudent = await Student.create()
-        // res.json(newStudent)
+        const newStudent = await Student.create({
+                id:req.body.id,
+                campusId:req.body.campusId,
+                firstName:req.body.firstName,
+                lastName:req.body.lastName,
+                gpa:req.body.gpa,
+                img:req.body.img
+            })
+        res.json(newStudent)
     } catch(err){
         console.log(err)
     }
 })
+
+router.get('/:id', async(req, res) => {
+    try {
+      const student = await Student.findByPk(req.params.id)
+      const campus = await Campus.findOne({where : {id:student.campusId}})
+      res.status(200).json({
+        student:student,
+        campus: campus
+      })
+    //   res.send(student)
+    } catch (error) {
+      res.send(error.message)
+    }
+  }) 
 
 module.exports = router
