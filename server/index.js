71a53c82ac04fc7edf060481301b37e98b43c0bd
Zixin Zou <zxinmm@gmail.com>
2022-01-25 11:25:02 -0500

    port modified

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT
 const morgan = require('morgan')
 const { db } = require('./database/index')
 const cors = require('cors')
